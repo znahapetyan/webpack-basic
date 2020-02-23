93c5f74d7f43ed17a3d2ff97ac79f9b50a5bbc80
Zachar Nahapetyan <znahapetyan@gmail.com>
2020-02-24 00:36:27 +0400

    Init

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import './style.scss';
+
+function component() {
+    const element = document.createElement('div');
+
+    element.innerHTML = 'Hello';
+    element.classList.add('hello');
+
+    return element;
+}
+
+document.body.appendChild(component());
---
+++ webpack.common.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: {
+        app: './src/index.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: ['file-loader'],
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: ['file-loader'],
+            },
+        ],
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'Production',
+        }),
+    ],
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+};
